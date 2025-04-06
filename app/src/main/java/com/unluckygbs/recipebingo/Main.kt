44d69ca34dbef531d44674080d7b745e6c6002ec
yuzid <yazid.fauzan9999@gmail.com>
2025-04-06 16:07:32 +0700

    init nav bar

--- app/src/main/java/com/unluckygbs/recipebingo/Main.kt
+++ app/src/main/java/com/unluckygbs/recipebingo/Main.kt
@@ -1,13 +1,39 @@
 package com.unluckygbs.recipebingo
 
+import androidx.compose.foundation.content.MediaType.Companion.Text
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.unluckygbs.recipebingo.screen.HomeScreen
+import com.unluckygbs.recipebingo.screen.IngredientScreen
 import com.unluckygbs.recipebingo.screen.LoginScreen
+import com.unluckygbs.recipebingo.screen.NutritionTrackerScreen
+import com.unluckygbs.recipebingo.screen.ProfileScreen
 import com.unluckygbs.recipebingo.screen.RegisterScreen
+import com.unluckygbs.recipebingo.screen.SearchRecipeScreen
 
 @Composable
 fun Main(modifier: Modifier = Modifier, authViewModel: AuthViewModel) {
@@ -21,7 +47,59 @@ fun Main(modifier: Modifier = Modifier, authViewModel: AuthViewModel) {
             RegisterScreen(modifier,navController,authViewModel)
         }
         composable("home"){
-            HomeScreen(modifier,navController,authViewModel)
+            App(modifier,navController,authViewModel)
         }
     })
+}
+
+@Composable
+fun App(modifier: Modifier = Modifier,navController: NavController, authViewModel: AuthViewModel) {
+
+    val navItemList = listOf(
+        NavItem("Home", Icons.Default.Home),
+        NavItem("Search", Icons.Default.Search),
+        NavItem("Stock", Icons.Default.Add),
+        NavItem("Track",Icons.Default.Check),
+        NavItem("Profile", Icons.Default.AccountCircle)
+    )
+
+    var selectedIndex by remember {
+        mutableIntStateOf(0)
+    }
+
+    Scaffold (
+        modifier = Modifier.fillMaxSize(),
+        bottomBar = {
+            NavigationBar {
+                navItemList.forEachIndexed{index, navItem ->
+                    NavigationBarItem(
+                        selected = selectedIndex == index,
+                        onClick = {
+                            selectedIndex = index
+                        },
+                        icon = {
+                            Icon(imageVector = navItem.icon, "icon")
+                               },
+                        label = {
+                            Text(text = navItem.label)
+                        }
+                    )
+
+                }
+            }
+        }
+        ) { innerPadding ->
+        ContentScreen(modifier = Modifier.padding(innerPadding), navController = navController, authViewModel = authViewModel,selectedIndex)
+    }
+}
+
+@Composable
+fun ContentScreen(modifier: Modifier = Modifier,navController: NavController, authViewModel: AuthViewModel,selectedIndex : Int) {
+    when(selectedIndex){
+        0 -> HomeScreen(modifier,navController,authViewModel)
+        1 -> SearchRecipeScreen(modifier,navController,authViewModel)
+        2 -> IngredientScreen(modifier,navController,authViewModel)
+        3 -> NutritionTrackerScreen(modifier,navController,authViewModel)
+        4 -> ProfileScreen(modifier,navController,authViewModel)
+    }
 }
---
+++ app/src/main/java/com/unluckygbs/recipebingo/screen/SearchRecipeScreen.kt
@@ -0,0 +1,39 @@
+package com.unluckygbs.recipebingo.screen
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.unluckygbs.recipebingo.AuthState
+import com.unluckygbs.recipebingo.AuthViewModel
+
+@Composable
+fun SearchRecipeScreen(modifier: Modifier = Modifier, navController: NavController, authViewModel: AuthViewModel) {
+    val authState = authViewModel.authState.observeAsState()
+
+    LaunchedEffect(authState.value) {
+        when(authState.value){
+            is AuthState.unAuthenticated -> navController.navigate("login")
+            else -> Unit
+        }
+    }
+
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Search Recipe", fontSize = 32.sp)
+
+        TextButton(onClick = {
+            authViewModel.signout()
+        }) { Text(text = "Log Out") }
+    }
+}
