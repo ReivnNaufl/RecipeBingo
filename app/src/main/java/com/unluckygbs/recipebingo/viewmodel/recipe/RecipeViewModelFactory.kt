63f6bd5bb2ced1238ad6691a78c4a63f9e5daed4
Muhammad Reivan N M <reivnnaufl@gmail.com>
2025-04-30 14:41:54 +0700

    base nutrition tracker done

---
+++ app/src/main/java/com/unluckygbs/recipebingo/viewmodel/recipe/RecipeViewModelFactory.kt
@@ -0,0 +1,4 @@
+package com.unluckygbs.recipebingo.viewmodel.recipe
+
+class RecipeViewModelFactory {
+}
---
+++ app/src/main/java/com/unluckygbs/recipebingo/viewmodel/tracker/NutritionTrackerViewModelFactory.kt
@@ -0,0 +1,4 @@
+package com.unluckygbs.recipebingo.viewmodel.tracker
+
+class NutritionTrackerViewModelFactory {
+}
